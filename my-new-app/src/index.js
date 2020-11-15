d43a24a578d9c35c8a7d4acf5ee98f18d23e0fc0
Pushpak Panchal <pushpak.net@gmail.com>
2020-11-14 22:15:53 -0600

    components reuse - adding comments

--- my-new-app/src/index.js
+++ my-new-app/src/index.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 //import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from "./serviceWorker";
 
 const App = () => {
-
   const btnText = "Click Me!";
-    return (
-      <div>
-       <label className="label" htmlFor="firstname">First name: </label>
-         <input type="text" id="firstname" />
-         <button type="button" style={{backgroundColor : 'blue', color : 'white'}} > { btnText}</button>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div>
+          <a href="/" className="author">
+            Same
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00 PM</span>
+          </div>
+
+          <div className="text">Nice blog post!</div>
+        </div>
       </div>
-      );
-}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
