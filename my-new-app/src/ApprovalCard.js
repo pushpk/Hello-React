97e444b1beb5c2dad9c0ecc906fe70b585cf017f
Pushpak Panchal <pushpak.net@gmail.com>
2020-11-15 19:05:54 -0600

    adding new project - module 04 - class based components

--- my-new-app/src/ApprovalCard.js
+++ my-new-app/src/ApprovalCard.js
@@ -1,28 +1,20 @@
 import React from "react";
-import faker from 'faker';
+import faker from "faker";
 
 const ApprovalCard = (props) => {
-    return (
-        
-        
-<div class="ui cards">
-  <div class="card">
-    <div class="content">
-      {props.children}
-    </div>
-    <div class="extra content">
-      <div class="ui two buttons">
-        <div class="ui basic green button">Approve</div>
-        <div class="ui basic red button">Decline</div>
+  return (
+    <div class="ui cards">
+      <div class="card">
+        <div class="content">{props.children}</div>
+        <div class="extra content">
+          <div class="ui two buttons">
+            <div class="ui basic green button">Approve</div>
+            <div class="ui basic red button">Decline</div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
-  
-    );
-
-
-
-}
+  );
+};
 
 export default ApprovalCard;
