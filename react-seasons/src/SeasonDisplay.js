b8612a3628d115d053d495a0da507da8294586d0
Pushpak Panchal <pushpak.net@gmail.com>
2020-11-16 00:29:18 -0600

    components - geo service

---
+++ react-seasons/src/SeasonDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+  return ( 
+    <div>Season Display there!</div>
+    );
+}
+
+
+export default SeasonDisplay;
