6367e49c6a7693696272011ca27bdaa69d0efdad
Pushpak Panchal <pushpak.net@gmail.com>
2020-11-17 00:13:00 -0600

    error handling

--- react-seasons/src/index.js
+++ react-seasons/src/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>Latitude: </div>
+//     );
+// }
 
-  window.navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude, position.coords.longitude);
-  },(err) => console.log(err));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = { lat: null, errorMessage : null };
 
-  return ( 
-    <div>Hi there!</div>
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) =>  this.setState({ errorMessage: err.message})
     );
+  }
+  render() {
+    if(this.state.errorMessage && !this.state.lat)
+    {
+      return <div>Error: {this.state.errorMessage}</div>;
+
+    }
+
+    if(!this.state.errorMessage && this.state.lat)
+    {
+      return <div>Latitude: {this.state.lat}</div>;
+      
+    }
+
+    return <div>Loading...</div>;
+  }
 }
 
-
-ReactDOM.render(<App /> ,document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById("root"));
