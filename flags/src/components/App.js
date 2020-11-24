671155e7c70f4c17c10928e4c847f937a0b2448e
Pushpak Panchal <pushpak.net@gmail.com>
2020-11-23 22:12:01 -0600

    adding this.context

--- flags/src/components/App.js
+++ flags/src/components/App.js
@@ -1,22 +1,30 @@
 import React from "react";
-import Usercreate from './UserCreate';
+import Usercreate from "./UserCreate";
+import LanguegeContext from "./contexts/LangaugeContext";
 
 class App extends React.Component {
   state = { language: "english" };
 
-  onLanguageChange = language =>{
-      this.setState({language : language});
-  }
+  onLanguageChange = (language) => {
+    this.setState({ language: language });
+  };
   render() {
     return (
       <div className='ui container'>
         <div>
           Select a language:
-          <i className='flag us' onClick={() =>  this.onLanguageChange('english')}/>
-          <i className='flag nl' onClick={() =>  this.onLanguageChange('dutch')}/>
+          <i
+            className='flag us'
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className='flag nl'
+            onClick={() => this.onLanguageChange("dutch")}
+          />
         </div>
-
-        <Usercreate />
+        <LanguegeContext.Provider value={this.state.language}>
+          <Usercreate />
+        </LanguegeContext.Provider>
       </div>
     );
   }
