9f8fb217d648e50accd4d20466e7a22a7773a5ce
Pushpak Panchal <pushpak.net@gmail.com>
2020-11-23 21:53:14 -0600

    adding context - this.context

--- flags/src/components/Button.js
+++ flags/src/components/Button.js
@@ -1,8 +1,12 @@
 import React from "react";
+import LanguegeContext from "./contexts/LangaugeContext";
 
 class Button extends React.Component {
+  static contextType = LanguegeContext;
+
   render() {
-    return <button className='ui button primary'>Submit</button>;
+    const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return <button className='ui button primary'>{text}</button>;
   }
 }
 
